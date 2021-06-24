f2d82632cde31945155a192597ec7e3525334921
marciobahia <marciosellabahia@hotmail.com>
2021-06-24 20:43:38 -0300

    Storage

---
+++ .history/src/configs/discordAuth_20210623213348.ts
@@ -0,0 +1,16 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-bf9ea97f-dac5c-bcad-49aef97418fa';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '857407314972377109';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
+
+
+
